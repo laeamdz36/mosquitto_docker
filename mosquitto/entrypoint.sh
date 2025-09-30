412916da8a4e686fe83a1303dd1e6f897d9ffa1c
luis MSK <luis3mendez6@gmail.com>
2025-09-30 15:47:03 -0600

    MOdify entrypoint and dockerfile

--- mosquitto/entrypoint.sh
+++ mosquitto/entrypoint.sh
@@ -1,28 +1,16 @@
 #!/bin/sh
-# set -e
-
-# PASSWD_FILE="/mosquitto/config/passwd"
-
-# if [ ! -f "$PASSWD_FILE" ]; then
-#     echo "🔑 Creando archivo de contraseñas..."
-#     mosquitto_passwd -c -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
-# else
-#     echo "🔑 Actualizando usuario existente..."
-#     mosquitto_passwd -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
-# fi
-
-# # Ejecutar el broker
-# exec mosquitto -c /mosquitto/config/mosquitto.conf
-#!/bin/sh
 set -e
 
 PASSWD_FILE="/mosquitto/config/passwd"
 
-# Create password if not exists
+# Crear archivo de passwords si no existe
 if [ ! -f "$PASSWD_FILE" ]; then
-    echo "Creating passwords..."
+    echo "🔑 Creando archivo de contraseñas..."
     mosquitto_passwd -c -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
+else
+    echo "🔑 Archivo de contraseñas existente, actualizando usuario..."
+    mosquitto_passwd -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
 fi
 
-# Start mosquitto with the already copied config
+# Ejecutar Mosquitto con la config
 exec mosquitto -c /mosquitto/config/mosquitto.conf
