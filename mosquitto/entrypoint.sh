0caf5b2f75b2021d85c0a18806d1f6d94b057e9a
luis MSK <luis3mendez6@gmail.com>
2025-09-30 14:55:44 -0600

    Modify entrypoint.sh

--- mosquitto/entrypoint.sh
+++ mosquitto/entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 PASSWD_FILE=/mosquitto/config/passwd
 
 if [ ! -f "$PASSWD_FILE" ]; then
-    mosquitto_passwd -c -b $PASSWD_FILE "$MQTT_USER" "$MQTT_PASS"
+    mosquitto_passwd -c -b $PASSWD_FILE $MQTT_USER $MQTT_PASS
     echo "Usuario $MQTT_USER creado en $PASSWD_FILE"
 fi
