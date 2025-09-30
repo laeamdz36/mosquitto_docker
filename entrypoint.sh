2c9d2ae928eddfecbf0036c8d5245824421f0d76
luis MSK <luis3mendez6@gmail.com>
2025-09-30 13:52:00 -0600

    Initial config docker compose and mosquitto

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+PASSWD_FILE=/mosquitto/config/passwd
+
+if [ ! -f "$PASSWD_FILE" ]; then
+    mosquitto_passwd -c -b $PASSWD_FILE "$MQTT_USER" "$MQTT_PASS"
+    echo "Usuario $MQTT_USER creado en $PASSWD_FILE"
+fi
