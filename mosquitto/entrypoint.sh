cc354066d6e4cc645630d7887ab274045fb504ee
luis MSK <luis3mendez6@gmail.com>
2025-09-30 15:35:59 -0600

    Modify entrypoint file

--- mosquitto/entrypoint.sh
+++ mosquitto/entrypoint.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
+# set -e
+
+# PASSWD_FILE="/mosquitto/config/passwd"
+
+# if [ ! -f "$PASSWD_FILE" ]; then
+#     echo "🔑 Creando archivo de contraseñas..."
+#     mosquitto_passwd -c -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
+# else
+#     echo "🔑 Actualizando usuario existente..."
+#     mosquitto_passwd -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
+# fi
+
+# # Ejecutar el broker
+# exec mosquitto -c /mosquitto/config/mosquitto.conf
+#!/bin/sh
 set -e
 
 PASSWD_FILE="/mosquitto/config/passwd"
 
+# Create password if not exists
 if [ ! -f "$PASSWD_FILE" ]; then
-    echo "🔑 Creando archivo de contraseñas..."
+    echo "Creating passwords..."
     mosquitto_passwd -c -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
-else
-    echo "🔑 Actualizando usuario existente..."
-    mosquitto_passwd -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
 fi
 
-# Ejecutar el broker
+# Start mosquitto with the already copied config
 exec mosquitto -c /mosquitto/config/mosquitto.conf
