5882feb605ebc4de5649b734c08d4b7ba3569dd6
luis MSK <luis3mendez6@gmail.com>
2025-10-01 11:57:54 -0600

    Add user inside container

---
+++ docs/README.md
@@ -0,0 +1,9 @@
+# Explanation of flow of execution to build Docker container
+
+1. Perform the configurations as usual for mosquitto borker in mosquitto/config/mosquitto.conf
+2. 
+
+# Notes
+
+- When host directory is mounted in the container mosquitto/config, 
+    apply chmod 0700 ./mosquitto/config
--- mosquitto/entrypoint.sh
+++ mosquitto/entrypoint.sh
@@ -4,8 +4,13 @@ set -e
 PASSWD_FILE="/mosquitto/config/passwd"
 
 # Crear archivo de passwords si no existe
-echo "🔑 Creando archivo de contraseñas..."
-mosquitto_passwd -c -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
+if [ ! -f "$PASSWD_FILE" ]; then
+    echo "🔑 Creando archivo de contraseñas..."
+    mosquitto_passwd -c -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
+else
+    echo "🔑 Archivo de contraseñas existente, actualizando usuario..."
+    mosquitto_passwd -b "$PASSWD_FILE" "$MQTT_USER" "$MQTT_PASS"
+fi
 
 # Ejecutar Mosquitto con la config
 exec mosquitto -c /mosquitto/config/mosquitto.conf
